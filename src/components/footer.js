f9b12b360c91bb44162480f9e561dd2a34676e75
Tyler Krys <Tyler.Krys@gov.bc.ca>
2022-06-27 10:48:04 -0700

    Replace <a> tag Home link with Gatsby <Link> component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import styled from "styled-components";
 
 const StyledFooter = styled.footer`
@@ -74,7 +75,7 @@ const Footer = () => (
       <nav>
         <ul>
           <li>
-            <a href=".">Home</a>
+            <Link to={"/"}>Home</Link>
           </li>
           <li>
             {/* CMS Lite permalink to https://www2.gov.bc.ca/gov/content/home/disclaimer */}
